60a92402e684da8a5cf3e493c1d0408abb0b8f2e
freen <freennnn@gmail.com>
2023-05-07 16:55:17 -0700

    feat: task #1 and task #2

---
+++ 01-read-file/index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+
+// process.cwd() => Absolute path of the current working directory.
+// path.join(process.cwd(), '01-read-file/text.txt')
+//const filePath = path.resolve('01-read-file/text.txt');
+const filePath = path.join(__dirname, 'text.txt');
+
+//printFile(filePath);
+//printFile2(filePath, onTextRead2);
+//printFile3(filePath, onAllTextRead3);
+printFile4(filePath);
+
+// async, most advanced with pipe (handles `backpressure` automatically)
+function printFile(filePath, encoding='utf8') {
+  console.log('Where is my file');
+  console.log(filePath);
+  fs.createReadStream(filePath, encoding).pipe(process.stdout);
+}
+
+// async, but the whole file
+function printFile2(filePath, callback) {
+  fs.readFile(filePath, 'utf8', (error, text) => {
+    if (error) {
+      console.error(error);
+      callback(null);
+      return;
+    }
+    callback(text);
+  })
+}
+
+function onTextRead2(text) {
+  console.log(text);
+}
+
+// stream with events (`flowing` mode, not `paused` one
+function printFile3(filePath, callback) {
+  let input = fs.createReadStream(filePath, 'utf-8');
+
+  input.on('data', (chunk) => {
+    console.log(chunk);
+    // console.log('next chunk');
+  });
+  input.on('end', () => {
+    callback(null);
+  });
+  input.on('error', err => {
+    callback(err);
+    process.exit();
+  });
+}
+
+function onAllTextRead3(err) {
+  if (!err) {
+    //console.log('File was printed successfully');
+  }
+}
+
+// stream - asynchronous iteration
+async function printFile4(filePath) {
+  let input = fs.createReadStream(filePath, 'utf-8');
+
+  // currently the file is small and the first chunk will contain the whole content. But for future
+  // we'll read and print content separating it by `new line`
+  let incompleteLine = '';
+  for await (let chunk of input) {
+    let lines = (incompleteLine + chunk).split('\n');
+    incompleteLine = lines.pop();
+    for (let line of lines) {
+      console.log(line);
+    }
+  }
+  console.log(incompleteLine);
+}
---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.resolve(__dirname, 'text.txt');
+//let output = fs.createWriteStream(filePath);
+let rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('Please enter your text:\n');
+rl.prompt();
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    isTimeToSayBye();
+  } else {
+    // {'flags': 'a'} used intentionally to append the file, remove it if you prefer to rewrite file every run
+    fs.appendFile(filePath, line + '\n', {'flags': 'a'}, writeCallback); 
+  }
+});
+
+function writeCallback(error) {
+  if (error) {
+    console.error(`file write operation failed with ${error}`);
+  }
+}
+
+const seeYaMessage = 'May the force be with you';
+function isTimeToSayBye() {
+  console.log(seeYaMessage);
+  process.exit();
+}
+
+process.on('SIGINT', function() {
+  isTimeToSayBye();
+});
+
+process.on('exit', (code) => {
+  rl.close();
+  console.log(`Arividerchi with ${code}`);
+});
