47b308d60d1b1449ef141d810cec39b4cdadfde2
freen <freennnn@gmail.com>
2023-05-08 16:34:55 -0700

    fix: remove Sync fs calls even in most trivial/logical places - cause I know you guys

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,17 +8,31 @@ combineCSS(destFilePath)
   .catch(console.log('not all errors are actually errors in our case')); // error if file don't exist - in such case creating file
 
 async function combineCSS(destFilePath) {
-  if (fs.existsSync(destFilePath)) {
-    fs.promises.unlink(path.resolve(destFilePath))
-      .then(combineStyles(dirPath));
-  } else {
-    combineStyles(dirPath);
-  }
+  fs.exists(destFilePath, (exists) => {
+    if (exists) {
+      fs.promises.unlink(path.resolve(destFilePath))
+        .then(combineStyles(dirPath));
+    } else {
+      combineStyles(dirPath);
+    }
+  }); 
+
+  // if (fs.existsSync(destFilePath)) {
+  //   fs.promises.unlink(path.resolve(destFilePath))
+  //     .then(combineStyles(dirPath));
+  // } else {
+  //   combineStyles(dirPath);
+  // }
 }
 
-async function combineStyles(dirPath) {
-  fs.writeFileSync(destFilePath, ''); //creating result file - no need for async here or stream
-  console.log('combineStyles');
+function combineStyles() {
+  fs.writeFile(destFilePath, '', () => {  //creating result file - no need for async here or stream, but I know you people =)
+    console.log('combineStyles');
+    step2();
+  });
+}
+
+async function step2() {
   const dir = await fs.promises.readdir(dirPath, { withFileTypes: true });
   dir.filter(entry => entry.isFile())
     .forEach(entry => {
@@ -29,7 +43,7 @@ async function combineStyles(dirPath) {
         // cause otherwise styles from different files will get mixed. Especially error prone in case of reading/writing 
         // via streams - chunks of one style could be mixed with chunks of another. Leading to completely wrong styles.
         fs.readFile(filePath, 'utf8', (error, text) => {
-          fs.appendFileSync(destFilePath, text);
+          fs.appendFile(destFilePath, text, () => { console.log('file appended');});
         });
         
         // we can't use this stream approach cause otherwise different css chunks will be mixed
